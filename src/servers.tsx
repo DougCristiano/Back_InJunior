6e297e40e79ce20f2fb6ddce6f3fba673a294f8e
Douglas Neves <douglascristiano@id.uff.br>
2026-02-20 13:04:58 -0300

    Chore: criando fastify

---
+++ src/servers.tsx
@@ -0,0 +1,12 @@
+import { app } from "./app.js";
+
+const host = "0.0.0.0";
+const port = 3000;
+
+app.listen({
+    host,
+    port,
+}).then(() => {
+    const url = `http://localhost:${port}`;
+    console.log(`Servidor rodando em ${url}`);
+})
